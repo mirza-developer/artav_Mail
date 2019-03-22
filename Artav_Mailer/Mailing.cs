771317d50c23d150da701e11f1bfe7bfa66fe9e6
ARTAV <artav.work@outlook.com>
2019-03-22 21:28:00 +0430

    Add project files.

---
+++ Artav_Mailer/Mailing.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Artav_Mailer.Management;
+
+namespace Artav_Mailer
+{
+    public class Mailing
+    {
+        public bool SendSmtp(MailMessage message, MailSetting setting)
+        {
+            try
+            {
+                if (!ValidateSettingBefore(setting))
+                    return false;
+                message.From = new MailAddress(setting.FromMailAddress);
+                message.IsBodyHtml = setting.IsBodyHtml;
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = setting.MailServiceUsername,
+                        Password = setting.MailServicePassword
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = setting.HostNameOrIp;
+                    smtp.Port = setting.Port;
+                    smtp.EnableSsl = setting.EnableSsl;
+                    //     smtp.Port = 8081;
+                    // smtp.EnableSsl = true;
+                    smtp.Send(message);
+
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> SendSmtpAsync(MailMessage message, MailSetting setting)
+        {
+            try
+            {
+                if (!ValidateSettingBefore(setting))
+                    return false;
+                message.From = new MailAddress(setting.FromMailAddress);
+                message.IsBodyHtml = setting.IsBodyHtml;
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = setting.MailServiceUsername,
+                        Password = setting.MailServicePassword
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = setting.HostNameOrIp;
+                    smtp.Port = setting.Port;
+                    smtp.EnableSsl = setting.EnableSsl;
+                    //     smtp.Port = 8081;
+                    // smtp.EnableSsl = true;
+                    smtp.Send(message);
+
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+
+        private bool ValidateSettingBefore(MailSetting passedInstance)
+        {
+            if (passedInstance.FromMailAddress == string.Empty)
+                return false;
+            if (passedInstance.MailServiceUsername == string.Empty)
+                return false;
+            if (passedInstance.MailServicePassword == string.Empty)
+                return false;
+            if (passedInstance.HostNameOrIp == string.Empty)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ Artav_Mailer/Management/MailSetting.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Artav_Mailer.Management
+{
+    public class MailSetting
+    {
+        public MailSetting()
+        {
+            this.FromMailAddress = string.Empty;
+            this.MailServiceUsername = string.Empty;
+            this.MailServicePassword = string.Empty;
+            this.HostNameOrIp = string.Empty;
+            this.IsBodyHtml = false;
+            this.EnableSsl = false;
+            this.Port = 8081;
+        }
+        public string FromMailAddress { get; set; }
+        public string MailServiceUsername { get; set; }
+        public string MailServicePassword { get; set; }
+        public string HostNameOrIp { get; set; }
+        public bool IsBodyHtml { get; set; }
+        public bool EnableSsl { get; set; }
+        public int Port { get; set; }
+    }
+}
